571c49b53ec91084b0517eae59c600e06ce1b34b
Thomas Staub <thomasstaub@wifi_stu-10-38-94-110.uncc.edu>
2018-06-19 18:12:18 -0400

    added delete orm

--- config/orm.js
+++ config/orm.js
@@ -1,44 +1,46 @@
 const connection = require("../config/connection.js");
 
 var orm = {
-    selectAll: function()  {
-        return new Promise((resolve,reject) =>  {
+    selectAll: function(cb)  {
+        
             connection.query("SELECT * FROM burgers", (err,res) =>  {
-                if(err) reject(err);
-                console.log(res);
-                resolve(res);
+                if(err) throw err;
+                cb(res);
             })
-        })
     },
-    insertOne: function(name)  {
-        console.log(name);
-        return new Promise((resolve,reject) =>  {
+    insertOne: function(name,cb)  {
             connection.query("INSERT INTO burgers SET ?",
             {
                 burger_name: name
             }, function(err,res)  {
-                if(err) reject(err);
-                resolve(res);
-                console.log(res);
+                if(err) throw err
+                cb(res);
             })
-        })
     },
-    updateOne: function(name)  {
-        return new Promise((resolve,reject) =>  {
+    updateOne: function(id,cb)  {
             connection.query("UPDATE burgers SET ? WHERE ?",
             [{
                 devoured: true
             },  {
-                burger_name: name
+                id: id
             }], function(err,res)  {
-                if(err) reject(err);
-                resolve(res);
-                console.log(res);
+                if(err) throw err
+                cb(res);
             })
+    },
+    throwTrash: () =>  {
+        connection.query("DELETE FROM burgers WHERE devoured=true", function(err,res)  {
+            if(err) throw err;
+        })
+    },
+    throwOne: (id,cb) =>  {
+
+        connection.query("DELETE FROM burgers WHERE id=?", [id], function(err,res)  {
+            if(err) throw err;
+            cb(res);
         })
     }
 }
-
 module.exports = orm;
 
 
